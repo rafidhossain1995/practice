01759643412b5189dbb312dd98333b8541c97eca
rafidhossain1995 <rafidhossain@pursuit.org>
2020-12-28 10:38:44 -0500

    work on common chars

--- dec.js
+++ dec.js
@@ -1,12 +1,34 @@
-const twoSum = (nums, target)=>{
-    const map = {}
-    for(let i =0; i<nums.length; i++){
-        const val = target - nums[i]
-        if(map[val] != undefined){
-            return [map[val], i]
+// function fizzBuzz(n) {
+//     // Write your code here
+//     for(let i=1; i <= 100; i++){
+//         if(n%3=== 0 && n%5 === 0){
+//            console.log("FizzBuzz") 
+//         } else if(n%3 === 0){
+//            console.log("Fizz") 
+//         } else if(n%5 === 0){
+//            console.log("buzz") 
+//         }else{
+//            console.log(n) 
+//         }
+//     }
+// }
+// fizzBuzz(100)
+
+
+
+var commonChars = function(a) {
+    let hash = {}
+    for(let i =0; i <= a.length; i++){
+        for(let key of a){
+            if(hash[key]){
+                hash[key]++
+            }else{
+                hash[key] = 1
+            }
         }
-        maps[nums[i]] = i
+        return hash
 
     }
-}
-console.log(twoSum(4, 3))
+   
+};
+console.log(commonChars(["hello"]))
